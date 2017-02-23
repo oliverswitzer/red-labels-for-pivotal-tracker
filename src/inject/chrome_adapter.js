b9abc92fdd0872e8376dd82867901c10b6f46e11
Oliver Switzer and Sai To Yeung <pair+oswitzer+syeung@pivotal.io>
2017-02-23 15:57:39 -0500

    WIP: how do we trigger modal to show?

--- src/inject/chrome_adapter.js
+++ src/inject/chrome_adapter.js
@@ -1,5 +1,17 @@
 chrome.extension.sendMessage({}, function() {
     setTimeout(function() {
+        var modalContainer = document.createElement("div");
+        modalContainer.innerHTML = `<div class="modal"><div class="modal-inner"><a data-modal-close>Close</a><div class="modal-content"></div></div></div>`;
+        var body = document.querySelector('body');
+        body.appendChild(modalContainer);
+
+        var modalContent = document.createElement("div");
+        modalContent.innerHTML = "<div id='wwltw-modal'>THIS IS MY CONTENT</div>";
+        body.appendChild(modalContent);
+
+        modal = new VanillaModal.default();
+        modal.open("#wwltw-modal");
+        debugger;
         storyListener(document);
     }, 2000);
 });
