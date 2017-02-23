96d720aabe4b086363f175ae6c8aff77d7ea57f7
Oliver Switzer and Sai To Yeung <pair+oswitzer+syeung@pivotal.io>
2017-02-23 16:56:49 -0500

    Entering a WWLTW now appends the response to an existing story with a title of “WWLTW”

--- src/wwltw_repository.js
+++ src/wwltw_repository.js
@@ -5,22 +5,49 @@ class WWLTWRepository {
     }
 
     add(projectId, submission) {
-        // WWLTWRepository.add(valeOfPrompt)
-        // --> find existing story by a hardcoded name
-        // --> format valueOfPrompt (?) to be list form
-        // --> update description
+        this.findExistingWWLTWStory(projectId)
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(responseJson) {
+                return responseJson[0];
+            })
+            .then(function(story) {
+                return this.updateWWLTWStory(story, submission, projectId);
+            }.bind(this))
+    }
 
-        fetch(this.TRACKER_BASE_URL + "/services/v5/projects/" + projectId + "/stories", {
-            method: 'POST',
+    updateWWLTWStory(story, submission, projectId) {
+        return fetch(this.generateApiUrlBase(projectId) + "/stories/" + story.id, {
+            method: 'PUT',
             headers: new Headers({
                 "X-TrackerToken": this.trackerApiToken,
                 "Content-Type": "application/json"
             }),
             body: JSON.stringify({
-                "current_state": "unscheduled",
-                "name": submission,
-                "story_type": "chore"
+                "description": story.description + '\n' + submission
             })
         });
+    }
+
+    findExistingWWLTWStory(projectId) {
+        return fetch(this.generateApiUrlBase(projectId) + "/stories?" + this.storySearchParameters(), {
+            method: 'GET',
+            headers: new Headers({
+                "X-TrackerToken": this.trackerApiToken,
+                "Content-Type": "application/json"
+            })
+        });
+    }
+
+    storySearchParameters() {
+        var urlSearchParam = new URLSearchParams();
+        urlSearchParam.append('filter', 'WWLTW');
+        urlSearchParam.append('limit', 1);
+        return urlSearchParam.toString();
+    }
+
+    generateApiUrlBase(projectId) {
+        return this.TRACKER_BASE_URL + "/services/v5/projects/" + projectId;
     }
 }
