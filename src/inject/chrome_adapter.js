25f39d9e2a29d4fd88a9d92f1fa1eb3a2118c073
Oliver Switzer and Sai To Yeung <pair+oswitzer+syeung@pivotal.io>
2017-02-23 18:34:49 -0500

    Change js prompt to a modal

--- src/inject/chrome_adapter.js
+++ src/inject/chrome_adapter.js
@@ -1,5 +1,28 @@
 chrome.extension.sendMessage({}, function() {
+
+    function addElementToBody(innerHtml) {
+        let modalContainer = document.createElement("div");
+        modalContainer.innerHTML = innerHtml;
+        document.querySelector('body').appendChild(modalContainer);
+    }
+
     setTimeout(function() {
-        storyListener(document);
+        addElementToBody('<div class="modal"><div class="modal-inner"><a data-modal-close>Close</a><div class="modal-content"></div></div></div>');
+        addElementToBody(`
+        <div id='wwltw-modal'>
+            <h3>What did you learn while working on this story?</h3>
+            <!--<label for="title">Title</label>-->
+            <!--<input type="text" id="title">-->
+            <form id="wwltw-form">
+                <div class="form-group">
+                    <label for="learning-body">Body</label>
+                    <textarea name="learning-body" id="learning-body" cols="30" rows="5"></textarea>
+                </div>
+                <input type="submit" value="Add to WWLTW story" onclick="">
+            </form>
+        </div>`);
+
+        let modal = new VanillaModal.default();
+        storyListener(document, modal);
     }, 2000);
 });
--- src/inject/story_listener.js
+++ src/inject/story_listener.js
@@ -1,24 +1,40 @@
-var storyListener = function (dom) {
+var storyListener = function (dom, modal) {
     chrome.storage.sync.get('trackerApiToken', function(options) {
-        var wwltwRepository = new WWLTWRepository(options.trackerApiToken);
+        let wwltwRepository = new WWLTWRepository(options.trackerApiToken);
 
-        bindListenersToFinishButtons(wwltwRepository);
+        bindListenersToFinishButtons(modal);
+        bindListenForSubmissionOfWWLTWForm(wwltwRepository, modal);
     });
 
+    let getProjectId = function () {
+        return /projects\/(\d*)/.exec(window.location)[1];
+    };
 
-    function bindListenersToFinishButtons(wwltwRepository) {
-        var finishButtons = dom.querySelectorAll('.button.finish');
-        var projectId = /projects\/(\d*)/.exec(window.location)[1];
+    function bindListenForSubmissionOfWWLTWForm(wwltwRepository, modal) {
+        let wwltwForm = dom.querySelector("#wwltw-form");
+        wwltwForm.addEventListener('submit', function (e) {
+            e.preventDefault();
+            let learningBody = e.target.querySelector("#learning-body");
+
+            wwltwRepository.add(getProjectId(), learningBody.value);
+
+            learningBody.value = "";
+            modal.close();
+        });
+    }
+
+    function bindListenersToFinishButtons(modal) {
+        let finishButtons = dom.querySelectorAll('.button.finish');
 
         finishButtons.forEach(function (button) {
             button.addEventListener('click', promptListener, false);
             function promptListener() {
-                var submission = prompt("What did you learn while working on this story?");
-
-                wwltwRepository.add(projectId, submission);
+                modal.open("#wwltw-modal");
 
                 button.removeEventListener('click', promptListener, false);
             }
         });
     }
+
+
 };
