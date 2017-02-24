2ecdddb9433391274240043256f67afb740fe956
Shawna Mattison <shawnamatt@gmail.com>
2017-02-24 11:55:28 -0500

    Modals be mad stylin, yo

--- src/inject/chrome_adapter.js
+++ src/inject/chrome_adapter.js
@@ -1,9 +1,4 @@
 chrome.extension.sendMessage({}, function () {
-    function addElementToBody(innerHtml) {
-        let modalContainer = document.createElement("div");
-        modalContainer.innerHTML = innerHtml;
-        document.querySelector('body').appendChild(modalContainer);
-    }
 
     setTimeout(function () {
         const modalInitializer = new WWLTWModal();
--- src/inject/wwltw_modal.js
+++ src/inject/wwltw_modal.js
@@ -9,20 +9,23 @@ class WWLTWModal {
                 </div>`;
 
         let modalContent = `
-            <div id='wwltw-modal'>
-                <h3>What did you learn while working on this story?</h3>
-                <form id="wwltw-form">
-                    <div class="form-group">
-                        <label for="learning-body">Body (markdown supported)</label>
-                        <textarea name="learning-body" id="learning-body" cols="30" rows="5"></textarea>
-                    </div>
+        <div id='wwltw-modal'>
+            <h3>What did you learn while working on this story?</h3>
+            <form id="wwltw-form" class="ui form">
+                <div class="field">
+                    <label for="learning-body">Body (markdown supported)</label>
+                    <textarea name="learning-body" id="learning-body" cols="30" rows="5"></textarea>
+                </div>
+                <div class="field">
+                    <label for="tags">Tags</label>
                     <select name="tags" multiple="" class="ui fluid search dropdown">
-                                  <option value="">Tags</option>
-                                  ${this.populateTags()}
-                              </select>
-                    <input type="submit" value="Add to WWLTW story" onclick="">
-                </form>
-            </div>`;
+                      <option value="">e.g. java, rails, tdd</option>
+                      ${this.populateTags()}
+                    </select>
+                </div>
+                <input type="submit" value="Add to WWLTW story" onclick="">
+            </form>
+        </div>`;
 
         this.addElementToBody(modalContainer);
         this.addElementToBody(modalContent);
