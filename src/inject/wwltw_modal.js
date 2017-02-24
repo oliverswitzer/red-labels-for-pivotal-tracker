a79ee9d41fd7a9049041eecb3c4e762ff241e47f
Oliver Switzer and Sai To Yeung <pair+oswitzer+syeung@pivotal.io>
2017-02-24 09:58:09 -0500

    Adds tags functionality without persistence
    NOTES:
    - Pulled out WWLTWModal class from chrome_adapter.js
    - Tags don’t get saved yet

---
+++ src/inject/wwltw_modal.js
@@ -0,0 +1,48 @@
+class WWLTWModal {
+    constructor() {
+        let modalContainer = `
+                <div class="modal">
+                    <div class="modal-inner">
+                        <a data-modal-close>Close</a>
+                        <div class="modal-content"></div>
+                    </div>
+                </div>`;
+
+        let modalContent = `
+            <div id='wwltw-modal'>
+                <h3>What did you learn while working on this story?</h3>
+                <form id="wwltw-form">
+                    <div class="form-group">
+                        <label for="learning-body">Body (markdown supported)</label>
+                        <textarea name="learning-body" id="learning-body" cols="30" rows="5"></textarea>
+                    </div>
+                    <select name="tags" multiple="" class="ui fluid search dropdown">
+                                  <option value="">Tags</option>
+                                  ${this.populateTags()}
+                              </select>
+                    <input type="submit" value="Add to WWLTW story" onclick="">
+                </form>
+            </div>`;
+
+        this.addElementToBody(modalContainer);
+        this.addElementToBody(modalContent);
+    }
+
+    populateTags() {
+        return tagList.map((tag) => {
+            return `<option value="${tag}">${tag}</option>`;
+        }).join('');
+    }
+
+    initialize() {
+        $('.ui.fluid.dropdown').dropdown();
+
+        return new VanillaModal.default();
+    }
+
+    addElementToBody(innerHtml) {
+        let modalContainer = document.createElement("div");
+        modalContainer.innerHTML = innerHtml;
+        document.querySelector('body').appendChild(modalContainer);
+    }
+}
