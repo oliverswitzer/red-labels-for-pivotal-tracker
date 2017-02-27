e136a0c54d506558e057b3e5f931bb727ca4a47b
Shawna Mattison <shawnamatt@gmail.com>
2017-02-27 12:10:50 -0500

    Add package and deployment scripts
    - Also includes a config file for CircleCI
    - Deployment is via the Google Developers API

---
+++ scripts/package.js
@@ -0,0 +1,42 @@
+#! /usr/bin/env node
+
+var fs = require('fs');
+var exec = require('child_process').exec;
+var jsonfile = require('jsonfile');
+
+var manifestFile = 'manifest.json';
+
+fs.readFile(manifestFile, 'UTF-8', function (err, data) {
+    if (err) {
+        console.log('Error!', err);
+    }
+    var manifest = JSON.parse(data);
+    var fileList = [
+        '_locales/**/*',
+        'icons/*'
+    ];
+
+    if(process.env.CIRCLE_BUILD_NUM) {
+        manifest.version = manifest.version + '.' + process.env.CIRCLE_BUILD_NUM;
+    }
+
+    jsonfile.writeFile('dist/' + manifestFile, manifest);
+
+    manifest.content_scripts.forEach(function (scripts) {
+        (scripts.css || scripts.js).forEach(function (file) {
+            fileList.push(file);
+        });
+    });
+
+    var zipFile = 'dist/' + manifest.short_name + '-' + manifest.version + '.zip';
+    fileList.unshift(zipFile);
+    var cmd = 'zip ' + fileList.join(' ') + ' && zip -j ' + zipFile + ' dist/manifest.json';
+
+    exec(cmd, function(error, stdout, stderr) {
+        if (error) {
+            console.log('Error!', error, stderr);
+        } else {
+            console.log('All good! Chrome extension packaged into\n', zipFile);
+        }
+    });
+});
