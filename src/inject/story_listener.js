b1dac0c093f51150f1a142006b40b051d8acec1e
Katie Hoffman and Shawna Mattison <pair+kahoffman+smattison@pivotal.io>
2017-02-28 10:14:51 -0500

    Attach event listeners to all finish buttons
    - Atttaching jQuery delegated event listener to the document rather than body targets ALL finish buttons in one foul swoooooop

--- src/inject/story_listener.js
+++ src/inject/story_listener.js
@@ -1,56 +1,5 @@
 var storyListener = function (modal) {
-
-    var observer = new MutationObserver(function (mutations) {
-        mutations.forEach(handleMutationEvents);
-    });
-
-    var handleMutationEvents = function handleMutationEvents(mutation) {
-        Array.prototype.forEach.call(mutation.addedNodes, addListenersInNode);
-        addListenersInNode(mutation.target);
-    }
-
-    var addListenersInNode = function styleLabelsInNode(node) {
-        if (nodeIsElement(node)) {
-            console.log(findFinishButtons(node));
-            addListenerToButtons(findFinishButtons(node));
-        }
-    }
-
-    var nodeIsElement = function nodeIsElement(node) {
-        return (typeof node.querySelectorAll !== 'undefined');
-    }
-
-    var findFinishButtons = function findLabelsInNode(node) {
-        return node.querySelectorAll('.button.finish.autosaves');
-    }
-
-    var addListenerToButtons = function addListenerToButtons(buttons) {
-        Array.prototype.forEach.call(buttons, function(button) {
-            button.addEventListener('click', promptListener, false);
-            function promptListener() {
-                modal.modal('show');
-                button.removeEventListener('click', promptListener, false);
-            }
-        });
-    }
-
-    var readyStateCheckInterval = setInterval(function() {
-        if (document.readyState === "complete") {
-            clearInterval(readyStateCheckInterval);
-
-
-            var config = {
-                attributes: true,
-                characterData: true,
-                childList: true,
-                subtree: true
-            };
-
-            observer.observe(document, config);
-        }
-    }, 10);
-
-    $('body').on('click', '.finish.button', function (e) {
+    $(document).on('click', '.finish.button', function () {
         modal.modal('show')
     })
 };
