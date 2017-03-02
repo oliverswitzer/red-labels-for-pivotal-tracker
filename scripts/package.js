033de5c6c5ec413a29016f95452252300e4a61e4
Oliver Switzer and Shawna Mattison <pair+oswitzer+smattison@pivotal.io>
2017-03-02 15:14:09 -0500

    Fix options script not loading correctly. Bump verison to 1.3.6

--- scripts/package.js
+++ scripts/package.js
@@ -23,7 +23,7 @@ fs.readFile(manifestFile, 'UTF-8', function (err, data) {
     jsonfile.writeFile('dist/' + manifestFile, manifest);
 
     fileList.push(manifest.options_page);
-    fileList.push('src/index.js');
+    fileList.push('src/options/index.js');
 
     manifest.content_scripts.forEach(function (scripts) {
         (scripts.css.concat(scripts.js)).forEach(function (file) {
