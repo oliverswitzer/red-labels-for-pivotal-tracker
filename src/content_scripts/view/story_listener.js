aceaf593278ae8c26e9d98a2e0088973879ba258
Katie Hoffman and Oliver Switzer <pair+kahoffman+oswitzer@pivotal.io>
2017-03-07 13:40:10 -0500

    Don't trigger modal when Finishing WWLTW Chore

--- src/content_scripts/view/story_listener.js
+++ src/content_scripts/view/story_listener.js
@@ -1,13 +1,46 @@
 import $ from 'jquery'
 
 export default (modal) => {
-    $(document).on('click', '.finish.button', function () {
-        setTimeout(function() {
-            chrome.runtime.sendMessage({ eventType: 'pop' });
+    $(document).on('click', '.finish.button', function (e) {
+        let storyTitle = extractStoryTitle(e.target);
 
-            modal.modal('show', function () {
-                modal.find('#learning-body').focus();
-            });
-        }, 0)
+        if(isTheWWLTWChore(storyTitle)) {
+            return false;
+        } else {
+            setTimeout(function() {
+                chrome.runtime.sendMessage({ eventType: 'pop' });
+
+                modal.modal('show', function () {
+                    modal.find('#learning-body').focus();
+                });
+            }, 0);
+        }
     });
 };
+
+const STORY_BUTTON_ID_REGEX = /story_state_button_([a-zA-Z0-9]+)_\w+/;
+
+function isTheWWLTWChore(storyTitle) {
+    return /WWLTW for the week of/.test(storyTitle);
+}
+
+function extractStoryTitle(target) {
+    if(isOpenedStory(target)) {
+        let storyId = /story_state_button_([a-zA-Z0-9]+)_\w+/.exec(target.id)[1];
+        return $(`.${storyId}`).find('.story.name textarea').val();
+    } else if (isCollapsedStory(target)) {
+        let storyNode = target.parentNode.parentNode;
+
+        return storyNode.querySelector('.story_name').innerText
+    }
+}
+
+function isCollapsedStory(target) {
+    let storyNode = target.parentNode.parentNode;
+
+    return storyNode.querySelector('.story_name') != null;
+}
+
+function isOpenedStory(target) {
+    return STORY_BUTTON_ID_REGEX.test(target.id);
+}
