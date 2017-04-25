289490e4cfd60a684cafb9569a0a079a7f68af2c
Oliver Switzer <pair+oswitzer@pivotal.io>
2017-04-25 18:03:43 -0400

    Refactor options/index.html to use React entirely

---
+++ src/options/components/OnboardingSection.jsx
@@ -0,0 +1,67 @@
+import React from 'react'
+
+export default class OnboardingSection extends React.Component {
+  constructor() {
+    super();
+
+
+    this.styles = {
+      subheader: {
+        marginBottom: '55px',
+        textAlign: 'center'
+      },
+      numberIcon: {
+        fontSize: '2em',
+        margin: '0 auto',
+        backgroundColor: '#138078',
+        color: '#FFFFFF',
+        borderRadius: '50%',
+        height: '100px',
+        width: '100px',
+        textAlign: 'center',
+        lineHeight: '100px'
+      },
+      howTo: {
+        textAlign: 'center'
+      },
+      howToImage: {
+        margin: '0 auto'
+      }
+    }
+  }
+
+  render() {
+    return (
+      <div className="ui three column grid">
+        <div className="column">
+          <div style={this.styles.howTo}>
+            <div style={this.styles.numberIcon}>1</div>
+            <h2 style={this.styles.subheader}>Finish a story and enter a learning.</h2>
+            <img style={this.styles.howToImage} src="static/create-a-learning.gif"
+                 className="ui wireframe image rounded how-to"/>
+          </div>
+        </div>
+        <div className="column">
+          <div style={this.styles.howTo}>
+            <div style={this.styles.numberIcon}>2</div>
+            <h2 style={this.styles.subheader}>We add the learning to an auto-generated chore for that week.</h2>
+            <img style={this.styles.howToImage} src="static/add-it-to-story.gif"
+                 className="ui wireframe image rounded how-to"/>
+          </div>
+        </div>
+        <div className="column">
+          <div style={this.styles.howTo}>
+            <div style={this.styles.numberIcon}>3</div>
+            <h2 style={this.styles.subheader}>Finish the chore and the tool composes your WWLTW email.</h2>
+
+            <img style={this.styles.howToImage} src="static/send-the-email.gif"
+                 className="ui wireframe image rounded how-to"/>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+
+
--- src/options/main.jsx
+++ src/options/main.jsx
@@ -1,14 +1,52 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
-import ProjectToggleSection from "./components/ProjectToggleSection.jsx"
+import TrackerTokenForm from './components/TrackerTokenForm.jsx';
+import OnboardingSection from "./components/OnboardingSection.jsx";
 
 class Main extends React.Component {
-    render() {
-        return <h1></h1>
+  constructor() {
+    super();
+
+    this.styles = {
+      container: {
+        marginTop: '50px'
+      },
+      mainHeader: {
+        marginBottom: '55px',
+        textAlign: 'center'
+      }
     }
+  }
+
+  render() {
+    return (
+      <div style={this.styles.container} className="ui grid">
+        <div className="row">
+          <div className="one wide column"/>
+          <div className="fourteen wide column">
+            <h1 style={this.styles.mainHeader}>What We Learned This Week for Pivotal Tracker</h1>
+            <OnboardingSection/>
+          </div>
+        </div>
+
+        <div className="row centered">
+          <h2>To get started, we need your Pivotal Tracker token.</h2>
+        </div>
+        <div className="row centered">
+          <h4>You can find your token at the bottom of <a href="https://www.pivotaltracker.com/profile">your profile</a>.
+            We only use it to make new WWLTW chores every week.</h4>
+        </div>
+        <div className="row centered">
+          <div className="five wide column">
+            <TrackerTokenForm/>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 ReactDOM.render(
-  <Main />,
+  <Main/>,
   document.getElementById('container')
 );
